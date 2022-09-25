eb077fbf8059365f56e8e0dbb282f23b0d7b4d22
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2022-09-25 10:53:36 +1000

    chore: remove unneded commands

--- src/App.js
+++ src/App.js
@@ -5,26 +5,15 @@ import { ReactTerminal } from "react-terminal";
 
 const App = () => {
   const [theme, setTheme] = React.useState("matrix");
-  const [controlBar, setControlBar] = React.useState(true);
-  const [controlButtons, setControlButtons] = React.useState(true);
   const [prompt, setPrompt] = React.useState(">>>");
 
   const commands = {
     help: (
       <span>
         <strong>clear</strong> - clears the console. <br />
-        <strong>change_prompt &lt;PROMPT&gt;</strong> - Change the prompt of the
-        terminal. <br />
         <strong>change_theme &lt;THEME&gt;</strong> - Changes the theme of the
         terminal. Allowed themes - light, dark, material-light, material-dark,
         material-ocean, matrix and dracula. <br />
-        <strong>toggle_control_bar</strong> - Hides / Display the top control
-        bar. <br />
-        <strong>toggle_control_buttons</strong> - Hides / Display the top
-        buttons on control bar. <br />
-        <strong>evaluate_math_expression &lt;EXPR&gt;</strong> - Evaluates a
-        mathematical expression (eg, <strong>4*4</strong>) by hitting a public
-        API, api.mathjs.org.
       </span>
     ),
 
@@ -48,20 +37,12 @@ const App = () => {
       setTheme(theme);
     },
 
-    toggle_control_bar: () => {
-      setControlBar(!controlBar);
-    },
-
-    toggle_control_buttons: () => {
-      setControlButtons(!controlButtons);
-    },
-
-    evaluate_math_expression: async (expr) => {
-      const response = await fetch(
-        `https://api.mathjs.org/v4/?expr=${encodeURIComponent(expr)}`
-      );
-      return await response.text();
-    },
+    // evaluate_math_expression: async (expr) => {
+    //   const response = await fetch(
+    //     `https://api.mathjs.org/v4/?expr=${encodeURIComponent(expr)}`
+    //   );
+    //   return await response.text();
+    // },
 
     command: async (command) => {
       const response = await fetch(
@@ -90,8 +71,8 @@ const App = () => {
         <ReactTerminal
           prompt={prompt}
           theme={theme}
-          showControlBar={controlBar}
-          showControlButtons={controlButtons}
+          showControlBar={true}
+          showControlButtons={true}
           welcomeMessage={welcomeMessage}
           commands={commands}
         />
